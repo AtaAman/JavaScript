1f5d424dc6b2602f2dbaf4814d65e07bacccd1bc
AmanAta <95543969+AmanAta@users.noreply.github.com>
2023-09-08 14:38:20 +0530

    datatypesSummary

---
+++ 01_basics/04_dataTypesSummary.js
@@ -0,0 +1,27 @@
+// primitive
+
+// 7 types : String, Number, Boolean, Null, undefined, symbol, Bigint
+
+
+const name = "aman"
+const score = 100
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // undefined
+const id = Symbol('123')
+const Bigint = 6149064028560283n;
+
+// reference (non-primitive)
+
+//array,objevcts,functions
+
+const heres = [ "aman", "kamran", "rehan", "nawazish"];
+
+let myObj = {
+    name: "aman",
+    age : 22,
+}
+
+const myFunction = function(){
+    console.log("hello Everyone");
+}
