348c67b4dd98cfe7c40d5dfd61146d2c63760e66
AmanAta <95543969+AmanAta@users.noreply.github.com>
2023-09-08 10:49:42 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+
+
+
+"use strict"; // treat all js code as newer version
+// alert( 3 + 3 ) // we are using nodejs not browser
+
+let name = "aman"  // default string
+let age = 22  // number
+let isLoggedIn = true  // boolean
+
+//number
+//bigint
+//string -> ""
+//boolean
+//null 
+//undefined
+//symbol
+
+//object
+
+//console.log(typrof undefined); // undefined
+console.log(typeof null);  // 
+
+
